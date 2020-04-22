7dfdefec76416f010debe337fbad82db40d17ee1
Okra Livia <okra.livia96@gmail.com>
2020-04-22 12:20:16 +0200

    other try device motion

--- index.js
+++ index.js
@@ -34,40 +34,50 @@ $(document).ready(function () {
             });
         });
 
-/*if ((window.DeviceMotionEvent) {
-  window.addEventListener('devicemotion', deviceMotionHandler, false);
-} else {
-  document.getElementById("dmEvent").innerHTML = "Not supported."
-}*/
 
-function deviceMotionHandler(eventData) {
-  var info, xyz = "[X, Y, Z]";
+    init();
 
-  // Grab the acceleration from the results
-  var acceleration = eventData.acceleration;
-  info = xyz.replace("X", acceleration.x);
-  info = info.replace("Y", acceleration.y);
-  info = info.replace("Z", acceleration.z);
-  document.getElementById("moAccel").innerHTML = info;
+    
+    function init() {
+      if ((window.DeviceMotionEvent) || ('listenForDeviceMovement' in window)) {
+        window.addEventListener('devicemotion', deviceMotionHandler, false);
+      } else {
+        document.getElementById("dmEvent").innerHTML = "Not supported on your device or browser.  Sorry."
+      }
+    }
+    
+    function deviceMotionHandler(eventData) {
+      var info, xyz = "[X, Y, Z]";
 
-  // Grab the acceleration including gravity from the results
-  acceleration = eventData.accelerationIncludingGravity;
-  info = xyz.replace("X", acceleration.x);
-  info = info.replace("Y", acceleration.y);
-  info = info.replace("Z", acceleration.z);
-  document.getElementById("moAccelGrav").innerHTML = info;
+      // Grab the acceleration including gravity from the results
+      var acceleration = eventData.acceleration;
+      info = xyz.replace("X", round(acceleration.x));
+      info = info.replace("Y", round(acceleration.y));
+      info = info.replace("Z", round(acceleration.z));
+      document.getElementById("moAccel").innerHTML = info;
 
-  // Grab the rotation rate from the results
-  var rotation = eventData.rotationRate;
-  info = xyz.replace("X", rotation.alpha);
-  info = info.replace("Y", rotation.beta);
-  info = info.replace("Z", rotation.gamma);
-  document.getElementById("moRotation").innerHTML = info;
+      // Grab the acceleration including gravity from the results
+      acceleration = eventData.accelerationIncludingGravity;
+      info = xyz.replace("X", round(acceleration.x));
+      info = info.replace("Y", round(acceleration.y));
+      info = info.replace("Z", round(acceleration.z));
+      document.getElementById("moAccelGrav").innerHTML = info;
 
-  // // Grab the refresh interval from the results
-  info = eventData.interval;
-  document.getElementById("moInterval").innerHTML = info;       
-}
+      // Grab the acceleration including gravity from the results
+      var rotation = eventData.rotationRate;
+      info = xyz.replace("X", round(rotation.alpha));
+      info = info.replace("Y", round(rotation.beta));
+      info = info.replace("Z", round(rotation.gamma));
+      document.getElementById("moRotation").innerHTML = info;
+
+      info = eventData.interval;
+      document.getElementById("moInterval").innerHTML = info;
+    }
+
+    function round(val) {
+      var amt = 10;
+      return Math.round(val * amt) /  amt;
+    }
 
 
 /*deviceMotionEvent.DeviceMotionEvent()
