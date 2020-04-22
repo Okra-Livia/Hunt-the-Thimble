0b8beff0f9ca7ea053e9c8587f865148a7a76ab8
Okra Livia <okra.livia96@gmail.com>
2020-04-22 12:41:16 +0200

    deviceOrientationHandler

--- index.js
+++ index.js
@@ -37,45 +37,43 @@ $(document).ready(function () {
 
 
     init();
-
+    var count = 0;
+    
     function init() {
-      if ((window.DeviceMotionEvent) || ('listenForDeviceMovement' in window)) {
-        window.addEventListener('devicemotion', deviceMotionHandler, false);
-        console.log("it works!");
+      if (window.DeviceOrientationEvent) {
+        document.getElementById("doEvent").innerHTML = "DeviceOrientation";
+        // Listen for the deviceorientation event and handle the raw data
+        window.addEventListener('deviceorientation', function(eventData) {
+          // gamma is the left-to-right tilt in degrees, where right is positive
+          var tiltLR = eventData.gamma;
+          
+          // beta is the front-to-back tilt in degrees, where front is positive
+          var tiltFB = eventData.beta;
+          
+          // alpha is the compass direction the device is facing in degrees
+          var dir = eventData.alpha
+          
+          // call our orientation event handler
+          deviceOrientationHandler(tiltLR, tiltFB, dir);
+          }, false);
       } else {
-        document.getElementById("dmEvent").innerHTML = "Not supported on your device or browser.  Sorry."
+        document.getElementById("doEvent").innerHTML = "Not supported on your device or browser.  Sorry."
       }
     }
+  
+    function deviceOrientationHandler(tiltLR, tiltFB, dir) {
+      document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
+      document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
+      document.getElementById("doDirection").innerHTML = Math.round(dir);
+      
+      // Apply the transform to the image
+      var logo = document.getElementById("imgLogo");
+      logo.style.webkitTransform = "rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
+      logo.style.MozTransform = "rotate("+ tiltLR +"deg)";
+      logo.style.transform = "rotate("+ tiltLR +"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
+    }
     
-    function deviceMotionHandler(eventData) {
-      var info, xyz = "[X, Y, Z]";
-
-      // Grab the acceleration including gravity from the results
-      var acceleration = eventData.acceleration;
-      info = xyz.replace("X", round(acceleration.x));
-      info = info.replace("Y", round(acceleration.y));
-      info = info.replace("Z", round(acceleration.z));
-      document.getElementById("moAccel").innerHTML = info;
-
-      // Grab the acceleration including gravity from the results
-      acceleration = eventData.accelerationIncludingGravity;
-      info = xyz.replace("X", round(acceleration.x));
-      info = info.replace("Y", round(acceleration.y));
-      info = info.replace("Z", round(acceleration.z));
-      document.getElementById("moAccelGrav").innerHTML = info;
-
-      // Grab the acceleration including gravity from the results
-      var rotation = eventData.rotationRate;
-      info = xyz.replace("X", round(rotation.alpha));
-      info = info.replace("Y", round(rotation.beta));
-      info = info.replace("Z", round(rotation.gamma));
-      document.getElementById("moRotation").innerHTML = info;
-
-      info = eventData.interval;
-      document.getElementById("moInterval").innerHTML = info;
-    }
-
-    function round(val) {
-      var amt = 10;
-      return Math.round(val * amt) /  amt;
-    }
+    
+    // Some other fun rotations to try...
+    //var rotation = "rotate3d(0,1,0, "+ (tiltLR*-1)+"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
+    //var rotation = "rotate("+ tiltLR +"deg) rotate3d(0,1,0, "+ (tiltLR*-1)+"deg) rotate3d(1,0,0, "+ (tiltFB*-1)+"deg)";
